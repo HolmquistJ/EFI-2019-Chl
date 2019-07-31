736faeab09452504f906e98bd151ac0be164139f
HolmquistJ <HolmquistJ@si.edu>
2019-07-31 15:18:17 -0400

    Added missing months as NA to wide form file.

--- scripts/wideFormCuration.R
+++ scripts/wideFormCuration.R
@@ -1,8 +1,20 @@
 library(tidyverse)
 
-chlWide <- read_csv("data/derivative/UMR_chl_tribs_bymonth_curated.csv") %>% 
+chl <- read_csv("data/derivative/UMR_chl_tribs_bymonth_curated.csv") %>% 
   mutate(fracYear = year + month/12) %>% # convert time to fractional years
-  select(Name, fracYear, CHLcal) %>%
+  select(Name, year, month, fracYear, CHLcal)
+
+targetYearDays <- expand.grid(year=min(chl$year):max(chl$year),
+                              month=1:12) %>%
+  arrange(year, month) %>%
+  mutate(fracYear = year + month/12,
+         Name=NA, CHLcal=NA) %>%
+  filter(! fracYear %in% chl$fracYear)
+
+chlWide <- chl %>%
+  bind_rows(targetYearDays) %>%
+  #select(Name, fracYear, CHLcal) %>%
   spread(key=Name, value=CHLcal)
 
 write_csv(chlWide, "data/derivative/UMR_chl_tribs_bymonth_curated_wideForm.csv")
+
