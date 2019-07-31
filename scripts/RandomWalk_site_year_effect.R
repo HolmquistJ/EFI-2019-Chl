a426d2da5cd00fc0300dc90fd5a9ccdeebffc14d
melofton <melofton@vt.edu>
2019-07-31 14:57:38 -0400

    model code w/ site for-loop; will likely need de-bugging!

--- scripts/RandomWalk_site_year_effect.R
+++ scripts/RandomWalk_site_year_effect.R
@@ -1,27 +1,33 @@
 #### Data Model
   model{
-  for(i in 1:N){
+    
+  for(i in 1:max_site){
+  
+    for(j in 1:max_year){
     #this fits the model to your observed data. 
     y[i] ~ dnorm(mu[i], tau_obs)
   }
 
    #### Process Model
-  for(i in 2:N){
-    mu[i]~dnorm(x[i],tau_add) #mus are the latent state (true chl-a)
-    x[i] <- x[i-1] + yr[year_no[i]] + site[site_no[i]]
+  
+    
+    for (j in 2:max_year){
+    mu[i,j]~dnorm(x[i,j],tau_add) #mus are the latent state (true chl-a)
+    x[i,j] <- x[i,j-1] + yr[year_no[j]] + site[site_no[i]]
+    }
   }
   
   #### Priors
-  x[1] ~ dgamma(x_ic,tau_ic) 
+  x[i,1] ~ dgamma(x_ic,tau_ic) 
   tau_add ~ dgamma(a_add,r_add)
   tau_obs ~ dgamma(a_obs, r_obs)
-  mu[1] ~ dgamma(0.01,0.01)
+  mu[i,1] ~ dgamma(0.01,0.01)
   tau_yr ~ dgamma(0.01,0.01)
   tau_site ~ dgamma(0.01,0.01)
   
   #Loops through number of years/sites and defines prior for each one 
-  for(i in 1:max_year) {
-    yr[i] ~ dnorm(0,tau_yr)
+  for(j in 1:max_year) {
+    yr[j] ~ dnorm(0,tau_yr)
   }
   
   for(i in 1:max_site) {
