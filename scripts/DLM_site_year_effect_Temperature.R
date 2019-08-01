22cda7cb1b72a91b4583ecaac21dc5163c537f71
melofton <melofton@vt.edu>
2019-08-01 13:38:37 -0400

    functional DLM w/ site&year effect == yay!

--- scripts/DLM_site_year_effect_Temperature.R
+++ scripts/DLM_site_year_effect_Temperature.R
@@ -6,7 +6,7 @@
     for(j in 1:N){
     #this fits the model to your observed data. 
     y[i,j] ~ dnorm(mu[i,j], tau_obs)
-      
+    Temp[i,j] ~ dnorm(temp[i,j],tau_obs_Temp)
   }
 
    #### Process Model
@@ -14,13 +14,14 @@
     
     for (j in 2:N){
     mu[i,j]~dnorm(x[i,j],tau_add) #mus are the latent state (true chl-a)
-    x[i,j] <- beta0 + beta1*x[i,j-1] + beta2*Temp[i,j] + yr[year_no[j]] + site[site_no[i]]
-    
+    x[i,j] <- beta[1] + beta[2]*x[i,j-1] + beta[3]*temp[i,j] + yr[year_no[j]] + site[site_no[i]]
+    temp[i,j] ~ dnorm(temp[i,j-1],tau_add_Temp)
     }
     
     x[i,1] ~ dgamma(x_ic,tau_ic) 
     mu[i,1] ~ dgamma(0.01,0.01)
     site[i] ~ dnorm(0,tau_site)
+    temp[i,1] ~ dnorm(0.5, 0.6)
   }
   
   #### Priors
@@ -28,6 +29,9 @@
   tau_obs ~ dgamma(a_obs, r_obs)
   tau_yr ~ dgamma(0.01,0.01)
   tau_site ~ dgamma(0.01,0.01)
+  beta ~ dmnorm(beta.m,beta.v)
+  tau_add_Temp ~ dgamma(a_add_Temp,r_add_Temp)
+  tau_obs_Temp ~ dgamma(a_obs_Temp, r_obs_Temp)
   
   #Loops through number of years/sites and defines prior for each one 
   #Loops through number of years and defines prior for each year 
--- scripts/Run_model.R
+++ scripts/Run_model.R
@@ -15,11 +15,22 @@ dat <- read_csv("./data/derivative/UMR_chl_tribs_bymonth_curated_wideForm.csv") 
   select(date, Apple:Wapsipinicon) %>%
   filter(year(date) != 1998) %>%
   select(-date)
+
+Temp <- read_csv("./data/derivative/UMR_temp_tribs_bymonth_curated_wideForm.csv") %>%
+  select(-fracYear) %>%
+  mutate(date = ymd(paste(year, month, 15, sep="-"))) %>%
+  select(date, Apple:Wapsipinicon) %>%
+  filter(year(date) != 1998) %>%
+  select(-date)
   
 #identify response variable and look at histogram
 y <- t(as.matrix(dat))
 hist(y)
 N = ncol(y)
+
+#identify covariate and look at histogram
+Temp <- t(as.matrix(Temp))
+hist(Temp)
 
 #data wrangling to get year_no for year effect and site_no for site effect
 years <- c(1999:2018)
@@ -31,7 +42,7 @@ sites <- unique(dat1$Name)
 site_no = as.numeric(as.factor(sites))
 
 ####Model - MUST MANUALLY TYPE IN THE CORRECT MODEL NAME HERE!!!
-model_name = 'RandomWalk_site_year_effect' 
+model_name = 'DLM_site_year_effect_Temperature' 
 model=paste0("scripts/",model_name, '.R') #Do not edit
 
 ###List correct inputs to JAGS for each model
@@ -57,6 +68,14 @@ variable.namesout.RandomWalk_site_year_effect<- c("tau_add", "mu", "tau_obs", "t
 init.RandomWalk_site_year_effect <- list(list(tau_add=0.001, tau_obs = 0.001, tau_site = 0.001, tau_yr = 0.001), list(tau_add=0.1, tau_obs = 0.1, tau_site = 0.1, tau_yr = 0.1), list(tau_add=1, tau_obs = 1, tau_site = 1, tau_yr = 1))
 params.RandomWalk_site_year_effect <- c("tau_add", "tau_obs","tau_site","tau_yr")
 
+#DLM w/ site-year effect
+data.DLM_site_year_effect_Temperature <- list(y=y,N = N, Temp = Temp, beta.m=as.vector(c(0,0,0)), beta.v=solve(diag(1E-03,3)), x_ic=20,tau_ic = 0.01, a_add = 0.001,r_add = 0.001, a_obs = 10, r_obs = 10, site_no=site_no, max_site = 9, year_no=year_no, max_year = 20, a_add_Temp = 0.001, r_add_Temp = 0.001, a_obs_Temp = 0.001, r_obs_Temp = 0.001)
+variable.names.DLM_site_year_effect_Temperature<- c("tau_add", "tau_obs", "tau_site", "tau_yr", "tau_add_Temp","tau_obs_Temp", "beta[1]","beta[2]","beta[3]")
+variable.namesout.DLM_site_year_effect_Temperature<- c("tau_add", "mu", "tau_obs", "tau_site","tau_yr", "tau_add_Temp","tau_obs_Temp", "beta[1]","beta[2]","beta[3]")
+init.DLM_site_year_effect_Temperature <- list(list(tau_add=0.01, tau_obs = 0.01, tau_site = 0.01, tau_yr = 0.01, beta=c(-0.5,-0.5,-0.5), tau_add_Temp = 0.01, tau_obs_Temp = 0.01), list(tau_add=0.1, tau_obs = 0.1, tau_site = 0.1, tau_yr = 0.1, beta=c(0,0,0), tau_add_Temp = 0.1, tau_obs_Temp = 0.1), list(tau_add=1, tau_obs = 1, tau_site = 1, tau_yr = 1,  beta=c(0.5,0.5,0.5), tau_add_Temp = 1, tau_obs_Temp = 1))
+params.DLM_site_year_effect_Temperature <- c("tau_add", "tau_obs","tau_site","tau_yr", "tau_add_Temp","tau_obs_Temp", "beta[1]","beta[2]","beta[3]")
+
+
 #get inputs to jags based on the model name you selected above
 data = eval(parse(text = paste0('data.', model_name)))
 variable.names = eval(parse(text = paste0('variable.names.', model_name)))
@@ -77,8 +96,8 @@ j.model   <- jags.model (file = model,
 #run model
 jags.out <- run.jags(model = model,
                      data = jags_plug_ins$data.model,
-                     burnin =  10000, 
-                     sample = 5000, 
+                     burnin =  2000, 
+                     sample = 2000, 
                      n.chains = 3, 
                      inits=jags_plug_ins$init.model,
                      monitor = jags_plug_ins$variable.namesout.model)
@@ -96,5 +115,32 @@ for (i in 1:length(params)){
 #get a matrix to work w/ for predictive intervals, etc.
 jags.out.mcmc <- as.mcmc.list(jags.out)
 out <- as.matrix(jags.out.mcmc)
+
+colnames(out[,2160:2169])
+
+dates <- dat <- read_csv("./data/derivative/UMR_chl_tribs_bymonth_curated_wideForm.csv") %>%
+  mutate(date = ymd(paste(year, month, 15, sep="-")))
+
+time <- as.Date(as.character(dates$date))
+times <- time[1:12]
+#time.rng = c(1,20) ## adjust to zoom in and out
+ciEnvelope <- function(x,ylo,yhi,...){
+  polygon(cbind(c(x, rev(x), x[1]), c(ylo, rev(yhi),
+                                      ylo[1])), border = NA,...) 
+}
+
+mus=grep("mu\\[9,", colnames(out))
+mu = out[,mus]
+ci <- apply(mu,2,quantile,c(0.025,0.5,0.975))
+
+
+png(file=file.path(my_directory,paste(site,paste0(model_name,'_CI_PI_2014.png'), sep = '_')), res=300, width=15, height=20, units='cm')
+plot(times,ci[2,1:12],type='n', ylab="Chl-a", ylim = c(min(ci[1,1:12]),max(ci[3,1:12])),main="Obs, Latent CI (blue), PI (green), Obs PI (grey)")
+ciEnvelope(times,ci[1,1:12],ci[3,1:12],col="lightBlue")
+ciEnvelope(times,obs_pi[1,],obs_pi[3,],col="gray")
+ciEnvelope(times,pi[1,],pi[3,],col="Green")
+points(times,y[9,1:12],pch="+",cex=0.8)
+points(times,obs_pi[2,],pch = 5, cex = 0.8)
+dev.off()
 
 
